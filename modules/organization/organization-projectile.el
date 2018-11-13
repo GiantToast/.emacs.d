84ca18567e5ba3cc4704bd6746cb42853fe2999f
Kyle Jones <ilovebees360@gmail.com>
2018-11-12 21:07:16 -0600

    Fixes regression in projectile helm keybindings

--- modules/organization/organization-projectile.el
+++ modules/organization/organization-projectile.el
@@ -1,4 +1,6 @@
 ;; Projectile
 (use-package projectile
   :ensure t
-  :config (projectile-global-mode t))
+  :config
+  (projectile-global-mode t)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
